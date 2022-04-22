1f9f2298448fca7b1077d50b9e3710769d70fbf0
Kuan-Shuo Liu <rex0707@gmail.com>
2022-04-22 15:16:10 +0800

    Add main.cpp
    Demonstrate the class Person.

--- source/main.cpp
+++ source/main.cpp
@@ -2,5 +2,18 @@
 
 int main( int argc, char* argv[] )
 {
+  Rex::Person Person1;
+  
+  Person1.ShowInfo();
+
+  Person1.SetName( "Parker Yang" );
+  Person1.SetAge( 28 );
+
+  Person1.ShowInfo();
+
+  Rex::Person Person2( "Rex Liu", 32 );
+
+  Person2.ShowInfo();
+
   return 0;
 }
